09d03f7b2fdc807e7a4b145cf363a1ceabbfbb02
Ahmed Waheed <ahm761171@gmail.com>
2024-12-11 16:42:06 +0200

    Update CategoryListingScreen to display a centered text widget

--- lib/features/category_listing/presentation/views/category_listing_screen.dart
+++ lib/features/category_listing/presentation/views/category_listing_screen.dart
@@ -5,6 +5,10 @@ class CategoryListingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text('Category Listing Screen'),
+      ),
+    );
   }
 }
